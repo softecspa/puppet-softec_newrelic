82abb49920cd326fab817f4aaf25963357c46c19
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-11-18 13:46:06 +0000

    modificato path di default

--- manifests/java.pp
+++ manifests/java.pp
@@ -1,12 +1,12 @@
 class softec_newrelic::java (
-  $newrelic_java_plugin_path  = '/opt',
+  $newrelic_java_plugin_path  = '/opt/newrelic',
   $newrelic_license_key       = hiera('newrelic_license_key'),
   $newrelic_java_appname      = undef,
   $java_version,
 ) {
 
   validate_absolute_path($newrelic_java_plugin_path)
-  
+
   $plugin_source = $java_version?{
     '6' => 'puppet:///modules/newrelic/java67_plugin/',
     '7' => 'puppet:///modules/newrelic/java67_plugin/',
